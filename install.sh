ee81f21d3bd9ae9c67407bd01983dab68780ab97
xiangnan <280145668@qq.com>
2022-10-23 15:37:04 +0000

    push

--- install.sh
+++ install.sh
@@ -6,6 +6,18 @@
 
 # chmod 700 ~/.ssh/*
 
+PROJECT_DIR=$PWD
+UBUNTU_VER=$(lsb_release -cs)
+OPENRESTY_VER=1.21.4.1
+OPENRESTY_DIR=/usr/local
+OPENRESTY_PATH="${OPENRESTY_DIR}/openresty/bin:${OPENRESTY_DIR}/openresty/nginx/sbin:${OPENRESTY_DIR}/openresty/luajit/bin"
+PATH="$OPENRESTY_PATH:$PATH"
+BASH_RC=~/.profile
+
+sudo sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 60/g' /etc/ssh/sshd_config
+sudo sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 60/g' /etc/ssh/sshd_config
+systemctl restart ssh
+
 install() {
   if [ -z $(which $1) ]; then
     sudo apt-get install -y $1
@@ -34,177 +46,189 @@ download_build_openresty() {
   make -j4 && make install
 }
 
-sudo sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 60/g' /etc/ssh/sshd_config
-sudo sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 60/g' /etc/ssh/sshd_config
-systemctl restart ssh
+install_base() {
+  sudo apt-get -q update
+  install build-essential
+  install unzip
+  install make
+  install gcc
+  install libpcre3-dev
+  install libssl-dev
+  install perl
+  install curl
+  install zlib1g
+  install zlib1g-dev
+}
 
-UBUNTU_VER=$(lsb_release -cs)
-# openresty
-sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates
-if [ ! -f /usr/share/keyrings/openresty.gpg ]; then
+install_nodejs() {
+  # nodejs
+  if [ -z $(which node) ]; then
+    curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && sudo apt-get install -y nodejs
+  fi
+  npm_global_install yarn
+  npm_global_install env-cmd
+}
+
+install_openresty() {
+  # openresty
+  sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates
+
   if [ $UBUNTU_VER = jammy ]; then
-    wget -O - https://openresty.org/package/pubkey.gpg | sudo gpg --dearmor -o /usr/share/keyrings/openresty.gpg
+    if [ ! -f /usr/share/keyrings/openresty.gpg ]; then
+      wget -O - https://openresty.org/package/pubkey.gpg | sudo gpg --dearmor -o /usr/share/keyrings/openresty.gpg
+    fi
   else
     wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
   fi
-fi
-if [ ! -f /etc/apt/sources.list.d/openresty.list ]; then
-  if [ $UBUNTU_VER = jammy ]; then
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/openresty.gpg] http://openresty.org/package/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list > /dev/null
+
+  if [ ! -f /etc/apt/sources.list.d/openresty.list ]; then
+    if [ $UBUNTU_VER = jammy ]; then
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/openresty.gpg] http://openresty.org/package/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list > /dev/null
+    else
+      echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"  | sudo tee /etc/apt/sources.list.d/openresty.list
+    fi
+  fi
+
+  if [ ! -z "$(grep openresty/bin $BASH_RC)" ]; then
+    echo "openresty path already written to ${BASH_RC}";
   else
-    echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"  | sudo tee /etc/apt/sources.list.d/openresty.list
+    echo "export PATH=$OPENRESTY_PATH:\$PATH" >> ${BASH_RC};
   fi
-fi
 
-
-# postgresql
-if [ -z "$(dpkg -l | grep -E '^ii\s+postgresql\s')" ]; then
-  # add postgresql latest
-  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-fi
-
-# nodejs
-if [ -z $(which node) ]; then
-  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-fi
-
-# dotnet-sdk-6.0
-if [ -z $(which dotnet) ]; then
-  wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-  sudo dpkg -i packages-microsoft-prod.deb
-  rm packages-microsoft-prod.deb
-fi
-
-# github cli
-if [ -z $(which gh) ]; then
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-fi
-
-sudo apt-get -q update
-
-install nodejs
-install unzip
-install make
-install gcc
-install libpcre3-dev
-install libssl-dev
-install perl
-install build-essential
-install curl
-install zlib1g
-install zlib1g-dev
-
-npm_global_install yarn
-npm_global_install env-cmd
-
-# install openresty
-PROJECT_DIR=$PWD
-OPENRESTY_VER=1.21.4.1
-OPENRESTY_DIR=/usr/local
-OPENRESTY_PATH="${OPENRESTY_DIR}/openresty/bin:${OPENRESTY_DIR}/openresty/nginx/sbin:${OPENRESTY_DIR}/openresty/luajit/bin"
-PATH="$OPENRESTY_PATH:$PATH"
-BASH_RC=~/.profile
-if [ ! -z "$(grep openresty/bin $BASH_RC)" ]; then
-  echo "openresty path already written to ${BASH_RC}";
-else
-  echo "export PATH=$OPENRESTY_PATH:\$PATH" >> ${BASH_RC};
-  echo "add $OPENRESTY_PATH to ${BASH_RC}"
-fi
-if [ ! -f ${OPENRESTY_DIR}/openresty/bin/openresty ]; then
-  # download_build_openresty $OPENRESTY_VER $OPENRESTY_DIR
-  sudo apt-get -y install openresty
-  # service openresty stop
-  # pgrep -x nginx && killall nginx
-  opm get ledgetech/lua-resty-http
-  opm get bungle/lua-resty-template
-  opm get fffonion/lua-resty-openssl
-  opm get xiangnanscu/pgmoon
-  opm get xiangnanscu/lua-resty-inspect
-else
-  echo "openresty already installed"
-fi
-
-# luarocks
-LUAROCKS_VER='3.9.1'
-LUAROCKS_FD=luarocks-${LUAROCKS_VER}
-LUAROCKS_GZ=${LUAROCKS_FD}.tar.gz
-if [ ! -f ${OPENRESTY_DIR}/openresty/luajit/bin/luarocks ]; then
-  cd /tmp
-  if [ ! -f $LUAROCKS_GZ ]; then
-    wget https://luarocks.org/releases/$LUAROCKS_GZ
+  sudo apt-get -q update
+  # install openresty
+  if [ ! -f ${OPENRESTY_DIR}/openresty/bin/openresty ]; then
+    # download_build_openresty $OPENRESTY_VER $OPENRESTY_DIR
+    sudo apt-get -y install openresty
+    # service openresty stop
+    # pgrep -x nginx && killall nginx
+  else
+    echo "openresty already installed"
   fi
-  if [ ! -d $LUAROCKS_FD ]; then
-    tar zxpf $LUAROCKS_GZ
+}
+
+install_luarocks() {
+  # luarocks
+  LUAROCKS_VER='3.9.1'
+  LUAROCKS_FD=luarocks-${LUAROCKS_VER}
+  LUAROCKS_GZ=${LUAROCKS_FD}.tar.gz
+  if [ ! -f ${OPENRESTY_DIR}/openresty/luajit/bin/luarocks ] && [ -f ${OPENRESTY_DIR}/openresty/bin/openresty ]; then
+    cd /tmp
+    if [ ! -f $LUAROCKS_GZ ]; then
+      wget https://luarocks.org/releases/$LUAROCKS_GZ
+    fi
+    if [ ! -d $LUAROCKS_FD ]; then
+      tar zxpf $LUAROCKS_GZ
+    fi
+    cd $LUAROCKS_FD
+    ./configure --prefix=${OPENRESTY_DIR}/openresty/luajit \
+        --with-lua=${OPENRESTY_DIR}/openresty/luajit/ \
+        --lua-suffix=jit \
+        --with-lua-include=${OPENRESTY_DIR}/openresty/luajit/include/luajit-2.1
+    make && make install
+    echo "luarocks ${LUAROCKS_VER} installed"
+  else
+    echo "luarocks already installed"
   fi
-  cd $LUAROCKS_FD
-  ./configure --prefix=${OPENRESTY_DIR}/openresty/luajit \
-      --with-lua=${OPENRESTY_DIR}/openresty/luajit/ \
-      --lua-suffix=jit \
-      --with-lua-include=${OPENRESTY_DIR}/openresty/luajit/include/luajit-2.1
-  make && make install
-  echo "luarocks ${LUAROCKS_VER} installed"
+}
 
-  # luarocks install  --tree lua_modules luasocket
-  cd $PROJECT_DIR
-  luarocks install luasocket
-  luarocks install luafilesystem
-  luarocks install luacheck
-  luarocks install lpeg
-  luarocks install ljsyscall
-  luarocks install argparse
-  luarocks install tl
-else
-  echo "luarocks already installed"
-fi
-
-if [ -z "$(dpkg -l | grep -E '^ii\s+postgresql\s')" ]; then
-  if [ -z $PG_PASSWORD ]; then
-    read -p "please provide postgresql password for user postgres:" PG_PASSWORD
+install_lua_packages() {
+  if [ ! -z $(which opm) ]; then
+    opm get ledgetech/lua-resty-http
+    opm get bungle/lua-resty-template
+    opm get fffonion/lua-resty-openssl
+    opm get xiangnanscu/pgmoon
+    opm get xiangnanscu/lua-resty-inspect
   fi
-  # install postgresql
-  sudo apt-get -y install postgresql
+  if [ ! -z $(which luarocks) ]; then
+    # luarocks install  --tree lua_modules luasocket
+    cd $PROJECT_DIR
+    luarocks install luasocket
+    luarocks install luafilesystem
+    luarocks install luacheck
+    luarocks install lpeg
+    luarocks install ljsyscall
+    luarocks install argparse
+    luarocks install tl
+  fi
+}
 
-  PG_VERSION_VERBOSE=`pg_config --version`
-  PG_VERSION=${PG_VERSION_VERBOSE:11:2}
+install_postgresql() {
+  # postgresql
+  if [ -z "$(dpkg -l | grep -E '^ii\s+postgresql\s')" ]; then
+    # add postgresql latest
+    if [ $UBUNTU_VER != jammy ]; then
+      sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+      wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+      sudo apt-get -q update
+    fi
+    # install postgresql
+    sudo apt-get -y install postgresql
 
-  echo "pg version: $PG_VERSION"
+    PG_VERSION_VERBOSE=`pg_config --version`
+    PG_VERSION=${PG_VERSION_VERBOSE:11:2}
 
-  # modify setting
-  sudo sed -i 's/max_connections = 100/max_connections = 400/g' /etc/postgresql/$PG_VERSION/main/postgresql.conf
-  sudo sed -i 's/shared_buffers = 128MB/shared_buffers = 256MB/g' /etc/postgresql/$PG_VERSION/main/postgresql.conf
-  sudo sed -i 's/#password_encryption = scram-sha-256/password_encryption = md5/g' /etc/postgresql/$PG_VERSION/main/postgresql.conf
-  sudo sed -i 's/\(\s\)scram-sha-256/\1md5/g' /etc/postgresql/$PG_VERSION/main/pg_hba.conf
+    echo "pg version: $PG_VERSION"
 
-  sudo service postgresql restart
-  sudo -u postgres psql -w postgres <<EOF
-    ALTER USER postgres PASSWORD '$PG_PASSWORD';
+    # modify setting
+    sudo sed -i 's/max_connections = 100/max_connections = 400/g' /etc/postgresql/$PG_VERSION/main/postgresql.conf
+    sudo sed -i 's/shared_buffers = 128MB/shared_buffers = 256MB/g' /etc/postgresql/$PG_VERSION/main/postgresql.conf
+    sudo sed -i 's/#password_encryption = scram-sha-256/password_encryption = md5/g' /etc/postgresql/$PG_VERSION/main/postgresql.conf
+    sudo sed -i 's/\(\s\)scram-sha-256/\1md5/g' /etc/postgresql/$PG_VERSION/main/pg_hba.conf
+    sudo service postgresql restart
+    if [ -z $PG_PASSWORD ]; then
+      read -p "please provide postgresql password for user postgres:" PG_PASSWORD
+    fi
+    sudo -u postgres psql -w postgres <<EOF
+      ALTER USER postgres PASSWORD '$PG_PASSWORD';
 EOF
 
-else
-  echo "postgresql already installed"
-fi
+  fi
+}
 
-# install dotnet-sdk-6.0
-if [ -z $(which dotnet) ]; then
-  wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-  sudo dpkg -i packages-microsoft-prod.deb
-  rm packages-microsoft-prod.deb
-  sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
-fi
 
-# install github cli
-if [ -z $(which gh) ]; then
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-  && sudo apt update \
-  && sudo apt install gh -y
-fi
+install_dotnet() {
+  # install dotnet-sdk-6.0
+  if [ -z $(which dotnet) ]; then
+    wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
+    sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
+  fi
+}
 
-git config --global user.email "280145668@qq.com"
-git config --global user.name "xiangnan"
-git config --global receive.denyCurrentBranch updateInstead
-git config --global receive.advertisePushOptions true
+install_gh() {
+  # github cli
+  if [ -z $(which gh) ]; then
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    && sudo apt update \
+    && sudo apt install gh -y
+  fi
+}
+
+config_git() {
+  git config --global user.email "280145668@qq.com"
+  git config --global user.name "xiangnan"
+  git config --global receive.denyCurrentBranch updateInstead
+  git config --global receive.advertisePushOptions true
+}
+
+install_base
+install_nodejs
+install_openresty
+install_luarocks
+install_postgresql
+install_dotnet
+install_gh
+install_lua_packages
+config_git
+
+
+
+
+
+
+
+
